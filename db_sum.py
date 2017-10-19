f855ddc9f76b5796cc2d1550b1126a45a2179ecd
striderdu <365240536@qq.com>
2017-10-20 00:22:43 +0800

    update db_sum

--- db_sum.py
+++ db_sum.py
@@ -1,80 +1,136 @@
 import os
 import re
 import math
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 lat_a = []
 ops_a = []
 med_a = []
-P99_a = []
+p99_a = []
 
-fr = open('perf_lat.csv','w')
-fr.write('IO mode,256B,1K,4K,16K,64K,256K\n')
-#fr.write(str(lat)+',')
-fr.close()
-fr = open('perf_med.csv','w')
-fr.write('IO mode,256B,1K,4K,16K,64K,256K\n')
-#fr.write(str(lat)+',')
-fr.close()
-fr = open('perf_ops.csv','w')
-fr.write('IO mode,256B,1K,4K,16K,64K,256K\n')
-#fr.write(str(lat)+',')
-fr.close()
-fr = open('perf_p99.csv','w')
-fr.write('IO mode,256B,1K,4K,16K,64K,256K\n')
-#fr.write(str(lat)+',')
-fr.close()
-param = ['fillseq','fillrandom','readseq','readrandom','updaterandom','overwrite']
+bench = ['fillseq','fillrandom','readseq','readrandom','updaterandom','overwrite']
 size = ['256','1024','4096','16384','65536','262144']
+system = ['PerfLog-ext4','PerfLog-btrfs','PerfLog-xfs','PerfLog-bluefs','PerfLog-rocksfs']
+param = ['iops','lat','med','p99']
+cparam = ['IOPS','LAT','MED','P99']
 
-for i in range(len(param)):
-    for j in range(len(size)):
-        path = 'log/PerfLog-bluefs/'+ param[i] +'_'+size[j]+'.log'
-        fp = open(path,'r')
-        #print (path)
-        content = fp.read()
-        #print (content)
-        #content='fillrandom   :    1988.861 micros/op 502 ops/sec;    0.1 MB/s fillrandom   :    2208.619 micros/op 452 ops/sec;    0.4 MB/s'
-        latP = re.compile(r'\w+\.\w+(?=\smicros/op\b)')
-#        latPfs = re.compile(r'(?<=\bfillseq\s\s\s\s\s\s:\s\s\s\s)\d+\.\d*|0\.\d*[1-9]\d*$')
-#        latPrr = re.compile(r'(?<=\bfillseq\s\s\s\s\s\s:\s\s\s\s)\d+\.\d*|0\.\d*[1-9]\d*$')
-        opsP = re.compile(r'(?<=\bop\s)\w+\b')
-        medP = re.compile(r'(?<=\bMedian:\s)\d+\.\d*|0\.\d*[1-9]\d*$')
-        P99P = re.compile(r'(?<=\bP99:\s)\d+\.\d*|0\.\d*[1-9]\d*$')
+#生成.csv文件
+def createCsv():
+    for i in range(len(param)):
+        for j in range(len(bench)):
+            lat_a = []
+            ops_a = []
+            med_a = []
+            p99_a = []
+            for k in range(len(system)):
+                for l in range(len(size)):
+                    path = 'log/'+system[k]+'/'+ bench[j] +'_'+size[l]+'.log'
+                    #print (path)
+                    fp = open(path,'r')
+                    #print (path)
+                    content = fp.read()
+                    #print (content)
+                    latP = re.compile(r'\w+\.\w+(?=\smicros/op\b)')
+                    opsP = re.compile(r'(?<=\bop\s)\w+\b')
+                    medP = re.compile(r'(?<=\bMedian:\s)\d+\.\d*|0\.\d*[1-9]\d*$')
+                    p99P = re.compile(r'(?<=\bP99:\s)\d+\.\d*|0\.\d*[1-9]\d*$')
+                    if (i==0):
+                        ops = int(opsP.findall(content)[0])
+                        ops_a.append(ops)
+                    elif (i==1):
+                        lat = round(float(latP.findall(content)[0])/1000,2)
+                        lat_a.append(lat)
+                    elif (i==2):
+                        med = round(1000000/float(medP.findall(content)[0]),2)
+                        med_a.append(med)
+                    elif (i==3):
+                        p99 = round(1000000/float(p99P.findall(content)[0]),2)
+                        p99_a.append(p99)
+            filename = 'csv/'+bench[j]+' '+param[i]+'.csv'
 
-        lat = round(float(latP.findall(content)[0])/1000,2)
-        #print (latP.findall(content))
-        #print (opsP.findall(content)[0])
-        ops = int(opsP.findall(content)[0])
-        med = round(float(medP.findall(content)[0]),2)
-        P99 = round(float(P99P.findall(content)[0]),2)
-        lat_a.append(lat)
-        ops_a.append(ops)
-        med_a.append(med)
-        P99_a.append(P99)
+            if i==0:
+                for u in range(0,30):
+                    if u%6==5:
+                        fr = open(filename,'a+')
+                        fr.write(str(ops_a[u])+'\n')
+                        fr.close()
+                    else:
+                        fr = open(filename,'a+')
+                        fr.write(str(ops_a[u])+',')
+                        fr.close()
+            elif i==1:
+                for u in range(0,30):
+                    if u%6==5:
+                        fr = open(filename,'a+')
+                        fr.write(str(lat_a[u])+'\n')
+                        fr.close()
+                    else:
+                        fr = open(filename,'a+')
+                        fr.write(str(lat_a[u])+',')
+                        fr.close()
+            elif i==2:
+                for u in range(0,30):
+                    if u%6==5:
+                        fr = open(filename,'a+')
+                        fr.write(str(med_a[u])+'\n')
+                        fr.close()
+                    else:
+                        fr = open(filename,'a+')
+                        fr.write(str(med_a[u])+',')
+                        fr.close()
+            elif i==3:
+                for u in range(0,30):
+                    if u%6==5:
+                        fr = open(filename,'a+')
+                        fr.write(str(p99_a[u])+'\n')
+                        fr.close()
+                    else:
+                        fr = open(filename,'a+')
+                        fr.write(str(p99_a[u])+',')
+                        fr.close()
 
-for i in range(0,35):
-    if i%6==0:
-        fr = open('perf_lat.csv','a+')
-        fr.write(param[i/6]+',')
-        fr.close()
-    else i:
-        fr
-        fr.write(lat_a[i%6+i/6]+'\n')
+    fp.close()
 
+#生成图表
+def createImg():
+    for l in range(len(bench)):
+        for k in range(len(param)):
+            path = 'csv/'+ bench[l] +' '+param[k]+'.csv'
+            L=[[float (x) for x in y.split(',')] for y in open(path).read().rstrip().split('\n')[0:]]
+            for i in range(1,5):
+                for j in range(6):
+                    L[i][j]=L[i][j]/L[0][j]
+            for j in range(6):
+                L[0][j]=1
+            size = 6
+            x = np.arange(6)
+            a = L[0]
+            b = L[1]
+            c = L[2]
+            d = L[3]
+            e = L[4]
+            labels = ['256B','1K','4K','16K','64K','256K'];
+            #调整画布大小
+            plt.figure(figsize=(13,8),dpi=80)
+            matplotlib.rcParams['xtick.direction'] = 'in'
+            matplotlib.rcParams['ytick.direction'] = 'in'
+            total_width, n = 0.5, 5
+            width = total_width / n
+            x = x - (total_width - width) / 5
 
+            plt.bar(x, a,  width=width, label='RocksDB-Ext4',ec='black', ls='-', lw=2,color='w',hatch='/')
+            plt.bar(x + width+0.03,b, width=width, label='RocksDB-Btrfs',ec='black', ls='-', lw=2,color='w',hatch='-')
+            plt.bar(x + 2 * width+0.06,c, width=width, label='RocksDB-XFS',ec='black', ls='-', lw=2, tick_label=labels,color='w',hatch='*')
+            plt.bar(x + 3 * width+0.09, d, width=width, label='RocksDB-BlueFS',ec='black', ls='-', lw=2,color='w',hatch='X')
+            plt.bar(x + 4 * width+0.12, e, width=width, label='RocksDB-RocksFS',ec='black', ls='-', lw=2,color='w',hatch='.')
+            #plt.bar(range(len(data)), data, ec='black', ls='-', lw=2)
+            plt.ylabel('Normalized '+cparam[k],fontsize=12)
+            plt.xlabel('Value Size',fontsize=12)
+            plt.grid(linewidth=0.5)
+            plt.legend(frameon='false',loc=0,fancybox='false')
+            plt.savefig('img/'+bench[l]+' '+param[k]+'.png')
 
-#print (lat)
-#print (ops)
-#print (med)
-print (lat_a)
-print (ops_a)
-print (med_a)
-print (P99_a)
-#fr = open('perf_med.csv','w')
-#fr.write('IO mode,256B,1K,4K,16K,64K,256K\n')
-#fr.write(str(lat)+',')
-#fr.close()
-
-#fr = open('perf_ops','w')
-#fr.write('IO mode,256B,1K,4K,16K,64K,256K\n')
-fp.close()
+createCsv()
+createImg()
